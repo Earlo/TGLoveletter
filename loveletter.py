225d3ba9e5cfd725085f6165cd193d5c7c74b342
Kasper Kivimäki <kasper.kivimaki@paivola.fi>
2017-10-13 23:41:00 +0300

    Class created for loveletter rules

---
+++ loveletter.py
@@ -0,0 +1,4 @@
+class LoveLetter:
+    cards = [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8]
+    def __init__(self, players):
+        # TODO Create list with size 'players' and give all a card from cards
