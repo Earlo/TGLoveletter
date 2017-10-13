5726c0bc0aba4e49618d58ea7f2d0cff49d2e688
Earlo <theearlord@gmail.com>
2017-10-14 00:34:27 +0300

    starting a game

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #from telegram.ext import Updater, CommandHandler
 from telegram.ext import Updater, CommandHandler
 import pprint
+import loveletter
+
 pp = pprint.PrettyPrinter(indent=4)
 
 from messages import *
 
-chatStates = {}
+games = {}
 
 #bot = telegram.Bot(token='468086505:AAGr_hubo_N0hjIR7ouUkZZvoHnhFl4ngr4')
 
@@ -19,8 +21,22 @@ def help(bot, update):
 	update.message.reply_text(HELP)
 
 def startGame(bot, update):
-	pp.pprint(dir(update))
-	update.message.reply_text(HELP)
+	#pp.pprint(dir(update.message))
+	if len(list(games.keys())) > 0:
+		nkey = max(list(games.keys())) + 1 
+	else:
+		nkey = 0
+	if len(update.message.text.split(" ")) >= 2:
+		pcount = int ( update.message.text.split(" ")[1] )
+		update.message.reply_text("Created game #{} with {} players".format(nkey, pcount))
+		print(nkey, pcount)
+	else:
+		update.message.reply_text("please specify player count")
+
+
+	#games[nkey] = loveletter.LoveLetter()
+	
+	#update.message.reply_text(update.message.text)
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
