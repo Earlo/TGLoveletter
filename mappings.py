f495b0628ab6401645b6dec49771973764d77745
Leo Tarpila <leo.tarpila@aalto.fi>
2017-10-14 05:41:26 +0300

    Mapping include numbers as strings

--- mappings.py
+++ mappings.py
@@ -6,14 +6,14 @@ CARDS = dict(map(
             return temp
         ),
         {
-            (1, "guard"): 1,
-            (2, "priest"): 2,
-            (3, "baron"): 3,
-            (4, "handmaid"): 4,
-            (5, "prince"): 5,
-            (6, "king"): 6,
-            (7, "countess"), 7,
-            (8, "princess"), 8
+            (1, "1", "guard"): 1,
+            (2, "2", "priest"): 2,
+            (3, "3", "baron"): 3,
+            (4, "4","handmaid"): 4,
+            (5, "5", "prince"): 5,
+            (6, "6", "king"): 6,
+            (7, "7", "countess"), 7,
+            (8, "8", "princess"), 8
         }.items()
     )
 )
